abd31453f7b9d6e9a2eb14430f7b74b482099610
pchessah <paulchesa1@gmail.com>
2020-04-23 17:02:35 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Login from "./components/customer/LogIn";
+import SignUp from "./components/customer/SignUp";
+import ConfirmDelivery from "./components/customer/ConfirmDelivery";
+import SizeSelection from "./components/customer/SizeSelection";
+import OrderConfirmed from "./components/customer/OrderConfirmed";
 
 function App() {
   return (
     <>
       <Navbar />
       <Switch>
-        <Route exact path ="/" component={Home} />
-        <Route path ="/customer_login" component={Login} />
-        <Route path ="/customer_signup" component={SignUp} />
-        <Route path ="/confirm_delivery" component={ConfirmDelivery} />
-        <Route path ="/size_selection" component={SizeSelection} />
-        <Route path ="/order_confirmed" component={OrderConfirmed} />
+        <Route exact path="/" component={Home} />
+        <Route path="/customer_login" component={Login} />
+        <Route path="/customer_signup" component={SignUp} />
+        <Route path="/confirm_delivery" component={ConfirmDelivery} />
+        <Route path="/size_selection" component={SizeSelection} />
+        <Route path="/order_confirmed" component={OrderConfirmed} />
       </Switch>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
 
-function Navbar() {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
+function NavigationBar() {
+
+  return (
+    <>
+      <Navbar className= "whole_navbar" collapseOnSelect expand="lg" bg="dark" variant="light">
+        <Navbar.Brand className= "nav_brand" to="/">Easeria Deliveries</Navbar.Brand>
+        <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="toggle_controls" id="responsive-navbar-nav toggle_controls">
+          <Nav className="mr-auto">
+           
+          </Nav>
+          <Nav>
+          <Link
+              style={{ padding: "1.5rem" }}
+              className="nav-Link"
+              to="/"
+            >
+              Home
+            </Link>
+              
+            <Link
+              style={{ padding: "1.5rem" }}
+              className="nav-Link"
+              to="/customer_login"
+            >
+              Log In
+            </Link>
+            <Link
+              style={{ padding: "1.5rem" }}
+              className="nav-Link"
+              to="/customer_signup"
+            >
+              Sign Up
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
 }
 
-export default Navbar;
+export default NavigationBar;
