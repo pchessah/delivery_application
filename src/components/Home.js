2f33378888a265e1e44d1f9f9c3a9a07b42b5100
pchessah <paulchesa1@gmail.com>
2020-04-25 21:10:54 +0300

    login homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,12 @@ function Home() {
       <div className="home_text">
         {" "}
         Get your goods delivered <br />
-        at affordable prices to your convinient location <br/>
+        at affordable prices to your convinient location. <br/>
+        <div className="arrow">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         <Link to="customer_signup">
         <button>Get Started </button>
         </Link>
--- src/components/customer/LogIn.js
+++ src/components/customer/LogIn.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function LogIn() {
-    return (
-        <div>
-            log in page
+  return (
+    <div>
+      <form className="signup">
+        <div className="container signupDiv">
+          <br />
+          <p>Log In</p>
+          <hr />
+
+          <label for="email">
+            <b>Email</b>
+          </label>
+          <input type="email" placeholder="Enter Email" name="email" required />
+
+          <label for="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            required
+          />
+
+          <label>
+            <input
+              type="checkbox"
+              checked="checked"
+              name="remember"
+              style={{ marginBottom: "15px" }}
+            />{" "}
+            Remember me
+          </label>
+
+          <Link to="/customer_signup">
+            <p style={{ color: "dodgerblue" }}>
+              Don't have an account? Sign Up{" "}
+            </p>
+          </Link>
+
+          <div class="clearfix">
+            <Link to="/">
+              <button type="submit" class="signupbtn">
+                LogIn
+              </button>
+            </Link>
+          </div>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
 export default LogIn;
