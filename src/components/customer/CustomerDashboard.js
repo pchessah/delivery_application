ffc4da8f9d8e33c319927ec95d9367040be6bfba
pchessah <paulchesa1@gmail.com>
2020-04-26 22:35:54 +0300

    customer dashboard

--- src/components/customer/CustomerDashboard.js
+++ src/components/customer/CustomerDashboard.js
@@ -9,10 +9,12 @@ function CustomerDashboard() {
         <section className="pickupsection">
           <h3 style={{paddingBottom:"1.3rem"}}>Delivery</h3>
           <Link to="/">
-            <div></div>
+            <div>
+              <img style={{borderRadius: "3rem"}} className="img-fluid" src="https://images.pexels.com/photos/38271/ipad-map-tablet-internet-38271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="delivery" />
+            </div>
           </Link>
-          <section style={{ marginTop: "2rem" }}>
-            <ul>
+          <section style={{ marginTop: "6.5rem" }}>
+            <ul style={{fontSize: "1.3rem"}}>
               <li>Set pickup location</li>
               <li>Set prefered pick up time</li>
               <li>Choose delivery location</li>
@@ -24,10 +26,12 @@ function CustomerDashboard() {
         <section className="vendorsection">
           <h3 style={{paddingBottom:"1.3rem"}}>View Vendors</h3>
           <Link to="/">
-            <div></div>
+            <div>
+              <img style={{borderRadius: "3rem"}}className="img-fluid" src="https://images.pexels.com/photos/1036857/pexels-photo-1036857.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="vendor"/>
+            </div>
           </Link>
-          <section style={{ marginTop: "2rem" }}>
-            <ul>
+          <section style={{ marginTop: "6.5rem" }}>
+            <ul style={{fontSize: "1.3rem"}}>
               <li>Select Items from vendors</li>
               <li>Pay for items</li>
               <li>Choose delivery location</li>
